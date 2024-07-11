fdd77745ed372d270264dea81993b0ed4db1b439
bhurtado <betty.hurtado@gmail.com>
2024-07-11 10:09:59 -0400

    add TaskApiTest

---
+++ src/gatling/java/taskapi/TaskApiTest.java
@@ -0,0 +1,39 @@
+
+package taskapi;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class TaskApiTest extends Simulation {
+        FeederBuilder.Batchable<String> feeder = csv("data/objects.csv").circular();
+
+        HttpProtocolBuilder httpProtocol = http
+                .baseUrl("https://api.restful-api.dev") // URL base
+                .acceptHeader("application/json") // Header para aceptar JSON
+                .contentTypeHeader("application/json"); // Header para enviar JSON
+
+        ScenarioBuilder scn = scenario("DemoTaskApi")
+                .feed(feeder)
+                .exec(http("POST Object")
+                        .post("/objects")
+                        .body(StringBody("{ \"name\": \"#{name}\" }"))
+                        .check(status().in(201, 200))
+                        .check(jsonPath("$.id").saveAs("objectId")))
+                .pause(1)
+                .exec(http("PUT Object")
+                        .put("/objects/#{objectId}")
+                        .body(StringBody("{\"name\": \"#{name} Updated\" }"))
+                        .check(status().in(200, 204)))
+                .pause(1)
+                .exec(http("GET Object")
+                        .get("/objects/#{objectId}")
+                        .check(status().is(200)));
+
+        {
+            setUp(scn.injectOpen(atOnceUsers(10)).protocols(httpProtocol));
+        }
+
+}
