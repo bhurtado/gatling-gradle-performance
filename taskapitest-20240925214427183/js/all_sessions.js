78af49f3228c490c84a24896ec29916367306507
bhurtado <betty.hurtado@gmail.com>
2024-09-25 21:44:34 +0000

    deploy: feae6c2e8b388b94b479f727fb3bb21bf4bacee1

---
+++ taskapitest-20240925214427183/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1727300668000,10],[1727300669000,10],[1727300670000,10],[1727300671000,10],[1727300672000,4]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
